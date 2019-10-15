fc8355f23ab78020dee50d5e2a92fd13be858b83
jordimahendra <50292090+jordimahendra@users.noreply.github.com>
2019-10-15 19:06:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<html>
+<head>
+<title>Lampung Cyber Security~</title>
+<meta charset='utf-8'/>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700' rel='stylesheet' type='text/css'>
+<style> body { background: black; font-family: Ubuntu Mono; color: white; text-align: center; font-size: 1.0em; } footer { position: fixed; left: 0px; right: 0px; bottom: 0px; background: white; text-align: center; border-top: 1px solid white; border-bottom: 1px solid white; } #s0 { text-align: left; opacity: 0; }</style>
+<body oncontextmenu='return false;' onkeydown='return false;'>
+</head>
+<font color="white" size="6">Lampung Cyber Security</font><br>
+<img src='https://3.top4top.net/s_13838csyz0.png' height='325'/><br />
+<font face="Ubuntu Mono"size="3">
+<font color="Lime">
+<font color="white">
+<br>
+Entah Apa Yang Merasuki Mu Wahai Pemimpin Negeri!
+<br><font color="white" size="3"> &copy CopyRight : Jordi.mahendra80gmail.com
+<br>
+<form>
+<br />shoot :<br /><font color="Lime" font face="Ubuntu Mono">&#12300;<marquee width="25%" behavior="alternate" scrollamount="5">
+| Mamank Garox | Mr.J | Mr.J | Pak Thanos | & Thanks You Lampung Security</marquee><font color="Lime">&#12301;<p/><br>
+</head>
+</html>
